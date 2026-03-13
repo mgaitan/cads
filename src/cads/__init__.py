37eb2cbca3721f2dafbb401cb2848427036754ca
Martín Gaitán <gaitan@gmail.com>
2026-03-12 21:45:05 -0300

    Actualiza helper de golas y capturas

---
+++ src/cads/__init__.py
@@ -0,0 +1,1 @@
+"""Shared helpers for CAD generation scripts."""
--- src/cads/freecad_gola.py
+++ src/cads/freecad_gola.py
@@ -31,6 +31,29 @@ def make_j_gola(doc, name, x, y, z, length):
     return add_compound(doc, name, shapes)
 
 
+def make_j_gola_inverted(doc, name, x, y, z, length):
+    y0 = y
+    shapes = [
+        Part.makeBox(
+            length, GOLA_TH, J_GOLA_TOTAL_H, App.Vector(x, y0 + J_GOLA_D - GOLA_TH, z)
+        ),
+        Part.makeBox(
+            length,
+            J_GOLA_D,
+            GOLA_TH,
+            App.Vector(x, y0, z + J_GOLA_TOTAL_H - GOLA_TH),
+        ),
+        Part.makeBox(length, GOLA_TH, 6.0, App.Vector(x, y0, z + J_GOLA_TOTAL_H - 6.0)),
+        Part.makeBox(
+            length,
+            6.0,
+            GOLA_TH,
+            App.Vector(x, y0 + J_GOLA_D - 6.0, z + J_GOLA_TOTAL_H - GOLA_TH - 6.0),
+        ),
+    ]
+    return add_compound(doc, name, shapes)
+
+
 def make_c_gola(doc, name, x, y, z, length):
     y0 = y
     lip_h = 10.0
